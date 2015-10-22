e518ff5c1df6abe0840ab75e0a11e184200ddbb8
Juan <cuentajuandelgado@gmail.com>
2015-10-21 20:19:03 -0430

    face
    1

---
+++ Assets/Extensions/StansAssetsPreviewUI/Scripts/SA_UI_Scripts/SASendMessageOnClick.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SASendMessageOnClick : SAOnClickAction {
+
+	public GameObject Reciver;
+	public string MethodName;
+	public bool useload=false;
+
+	void Start (){
+
+		if (useload) {
+			Reciver.SendMessage(MethodName, SendMessageOptions.DontRequireReceiver);
+		}
+	}
+
+
+	protected override void OnClick() {
+		Reciver.SendMessage(MethodName, SendMessageOptions.DontRequireReceiver);
+	}
+}
