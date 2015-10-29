1a84b8a34f7f80591c655dbb828371aef80b4e6a
jafasaj <danny.ruta949@gmail.com>
2015-10-29 05:52:51 -0430

    parallax test
    1

---
+++ Assets/Standard Assets/Side-Scroller Motorcycle/Textures/Fondo Ciudad Paralax/Paralaxcity.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paralaxcity : MonoBehaviour {
+
+	public float velocidad = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		gameObject.GetComponent<Renderer>().material.mainTextureOffset = new Vector2 (Time.time * velocidad, 0);
+	
+	}
+} 
