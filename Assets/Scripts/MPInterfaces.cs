03a882ebd1c7043190ca00c65b247c5e0edbbf27
Juan <cuentajuandelgado@gmail.com>
2015-11-01 06:04:51 -0430

    Implement RTM
    1

---
+++ Assets/Scripts/MPInterfaces.cs
@@ -0,0 +1,13 @@
+﻿public interface MPLobbyListener {
+	void SetLobbyStatusMessage(string message);
+	void HideLobby();
+}
+
+
+public interface MPUpdateListener {
+	void UpdateReceived(string participantId, float posX, float posY, float velX, float velY, float rotZ);
+	void PlayerFinished(string senderId, float finalTime);
+	void LeftRoomConfirmed();
+	void PlayerLeftRoom(string participantId);
+}
+
